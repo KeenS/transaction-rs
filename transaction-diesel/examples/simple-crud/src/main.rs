cac8dd498509844eb883a028027bc4cde18bfea0
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2017-06-17 17:18:26 +0900

    add comments to the example of transaction-diesel

--- transaction-diesel/examples/simple-crud/src/main.rs
+++ transaction-diesel/examples/simple-crud/src/main.rs
@@ -26,13 +26,20 @@ pub fn establish_connection() -> PgConnection {
 
 fn main() {
     let conn = establish_connection();
-    let tx = db::create_user("keen").and_then(move |user| {
+    // composed computation of DB operations
+    let tx = db::create_user("keen")
+    // Transactions can be sequenced using `and_then`
+        .and_then(move |user| {
         println!("created user: {:?}", user);
         db::update_user(user.id, "KeenS")
+                // to pass values by move, you need to inject them by `join`
             .join(ok(user))
             .and_then(|(res, user)| match res {
                 None => {
                     println!("user not found");
+                    // when you branch and return different `Transaction`s it is an error. Some operation is needed.
+                    // One option is boxing all the transactions returning from all the branches.
+                    // Another option is using `branch` API. Use `left` in one branch and `right` in the other branch.
                     ok(()).branch().left()
                 }
                 Some(()) => db::find_user(user.id)
@@ -59,5 +66,6 @@ fn main() {
             })
     });
 
+    // to run the composed computation, use `transaction_diesel::run`.
     transaction_diesel::run(&conn, tx).unwrap()
 }
