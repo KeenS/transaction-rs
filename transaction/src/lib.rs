648f879ce317f0aaf3d70d44bda84d6229c577b2
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2017-06-19 16:38:31 +0900

    add branch3, branch4

--- transaction/src/lib.rs
+++ transaction/src/lib.rs
@@ -182,6 +182,20 @@ pub trait Transaction {
         BranchBuilder(self)
     }
 
+    /// 3 branch builder
+    fn branch3(self) -> Branch3Builder<Self>
+        where Self: Sized
+    {
+        Branch3Builder(self)
+    }
+
+    /// 4 branch builder
+    fn branch4(self) -> Branch4Builder<Self>
+        where Self: Sized
+    {
+        Branch4Builder(self)
+    }
+
     // repeat
     // retry
 }
@@ -364,11 +378,11 @@ pub struct Join4<Tx1, Tx2, Tx3, Tx4> {
 pub struct BranchBuilder<Tx>(Tx);
 
 impl<Tx> BranchBuilder<Tx> {
-    pub fn left<B>(self) -> Branch<Tx, B> {
+    pub fn first<B>(self) -> Branch<Tx, B> {
         Branch::B1(self.0)
     }
 
-    pub fn right<B>(self) -> Branch<B, Tx> {
+    pub fn second<B>(self) -> Branch<B, Tx> {
         Branch::B2(self.0)
     }
 }
@@ -379,6 +393,69 @@ impl<Tx> BranchBuilder<Tx> {
 pub enum Branch<Tx1, Tx2> {
     B1(Tx1),
     B2(Tx2),
+}
+
+/// Branch3Builder
+#[derive(Debug)]
+#[must_use]
+pub struct Branch3Builder<Tx>(Tx);
+
+impl<Tx> Branch3Builder<Tx> {
+    pub fn first<B, C>(self) -> Branch3<Tx, B, C> {
+        Branch3::B1(self.0)
+    }
+
+    pub fn second<B, C>(self) -> Branch3<B, Tx, C> {
+        Branch3::B2(self.0)
+    }
+
+    pub fn third<B, C>(self) -> Branch3<B, C, Tx> {
+        Branch3::B3(self.0)
+    }
+}
+
+
+/// The result of `branch3`
+#[derive(Debug)]
+#[must_use]
+pub enum Branch3<Tx1, Tx2, Tx3> {
+    B1(Tx1),
+    B2(Tx2),
+    B3(Tx3),
+}
+
+/// Branch4Builder
+#[derive(Debug)]
+#[must_use]
+pub struct Branch4Builder<Tx>(Tx);
+
+impl<Tx> Branch4Builder<Tx> {
+    pub fn first<B, C, D>(self) -> Branch4<Tx, B, C, D> {
+        Branch4::B1(self.0)
+    }
+
+    pub fn second<B, C, D>(self) -> Branch4<B, Tx, C, D> {
+        Branch4::B2(self.0)
+    }
+
+    pub fn third<B, C, D>(self) -> Branch4<B, C, Tx, D> {
+        Branch4::B3(self.0)
+    }
+
+    pub fn fourth<B, C, D>(self) -> Branch4<B, C, D, Tx> {
+        Branch4::B4(self.0)
+    }
+}
+
+
+/// The result of `branch4`
+#[derive(Debug)]
+#[must_use]
+pub enum Branch4<Tx1, Tx2, Tx3, Tx4> {
+    B1(Tx1),
+    B2(Tx2),
+    B3(Tx3),
+    B4(Tx4),
 }
 
 /// The result of `join_vec`
@@ -652,6 +729,42 @@ impl<Tx1, Tx2> Transaction for Branch<Tx1, Tx2>
         match *self {
             Branch::B1(ref tx) => tx.run(ctx),
             Branch::B2(ref tx) => tx.run(ctx),
+        }
+    }
+}
+
+impl<Tx1, Tx2, Tx3> Transaction for Branch3<Tx1, Tx2, Tx3>
+    where Tx1: Transaction,
+          Tx2: Transaction<Ctx = Tx1::Ctx, Item = Tx1::Item, Err = Tx1::Err>,
+          Tx3: Transaction<Ctx = Tx1::Ctx, Item = Tx1::Item, Err = Tx1::Err>
+{
+    type Ctx = Tx1::Ctx;
+    type Item = Tx1::Item;
+    type Err = Tx1::Err;
+    fn run(&self, ctx: &mut Self::Ctx) -> Result<Self::Item, Self::Err> {
+        match *self {
+            Branch3::B1(ref tx) => tx.run(ctx),
+            Branch3::B2(ref tx) => tx.run(ctx),
+            Branch3::B3(ref tx) => tx.run(ctx),
+        }
+    }
+}
+
+impl<Tx1, Tx2, Tx3, Tx4> Transaction for Branch4<Tx1, Tx2, Tx3, Tx4>
+    where Tx1: Transaction,
+          Tx2: Transaction<Ctx = Tx1::Ctx, Item = Tx1::Item, Err = Tx1::Err>,
+          Tx3: Transaction<Ctx = Tx1::Ctx, Item = Tx1::Item, Err = Tx1::Err>,
+          Tx4: Transaction<Ctx = Tx1::Ctx, Item = Tx1::Item, Err = Tx1::Err>
+{
+    type Ctx = Tx1::Ctx;
+    type Item = Tx1::Item;
+    type Err = Tx1::Err;
+    fn run(&self, ctx: &mut Self::Ctx) -> Result<Self::Item, Self::Err> {
+        match *self {
+            Branch4::B1(ref tx) => tx.run(ctx),
+            Branch4::B2(ref tx) => tx.run(ctx),
+            Branch4::B3(ref tx) => tx.run(ctx),
+            Branch4::B4(ref tx) => tx.run(ctx),
         }
     }
 }
